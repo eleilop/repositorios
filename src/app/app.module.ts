200493ad5c54227c17bb2641dc472a1fb1202ad1
Juan A. García <juan.garcia@aliqindoi.com>
2024-10-08 10:05:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+// src/app/app.module.ts
+import { inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +7,27 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { PeopleRepositoryFactory } from './core/repositories/repository.factory';
+import { PeopleService } from './core/services/impl/people.service';
+import { PEOPLE_API_URL } from './core/repositories/repository.tokens';
+import { provideHttpClient } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient(),
+    { provide: PEOPLE_API_URL, useValue: 'https://randomuser.me/api/?results=100' },
+    // Registrar los repositorios
+    PeopleRepositoryFactory,
+    // Registrar otros repositorios según sea necesario
+    // Servicios de aplicación
+    {
+      provide: 'PeopleService',
+      useClass: PeopleService
+    }
+  ],
+  // ... otros proveedores],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
+import { inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers:[
+    provideHttpClient()
+  ]
 })
 export class HomePageModule {}
